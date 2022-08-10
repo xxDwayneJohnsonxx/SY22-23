9d028365e733dab35eb7dbbb497cbeb55a79bf65
Ayden Mack <armac417@learner.dvusd.org>
2022-08-10 09:29:51 -0700

    print initials

--- C#/ConsoleArt/ConsoleArt/Program.cs
+++ C#/ConsoleArt/ConsoleArt/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleArt
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("    a     m     m");
+            Console.WriteLine("  a   a   m m m m");
+            Console.WriteLine(" aaaaaaa  m  m  m");
+            Console.WriteLine("a       a m     m");
+            Console.ReadLine();
         }
     }
 }
