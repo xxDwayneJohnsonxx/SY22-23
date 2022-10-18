c22072514383bfc494863f097567acd9de44fe2a
Ayden Mack <armac417@learner.dvusd.org>
2022-10-18 08:52:54 -0700

    zlkll of them

---
+++ C#/pitbull305dale/pitbull305dale/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace pitbull305dale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Write("*");
+                }
+            WriteLine();
+            }
+              ReadKey();
+            for (int i = 0; i < 10; i++)
+            { 
+             for (int j = 0; j < 10; j++)
+            
+            
+            
+            }
+        }
+            
+    }
+}
