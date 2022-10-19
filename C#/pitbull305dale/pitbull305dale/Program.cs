978f15ad021babe86bc263f269ed1edf9d0ddc5e
Ayden Mack <armac417@learner.dvusd.org>
2022-10-19 08:45:48 -0700

    10/19

--- C#/pitbull305dale/pitbull305dale/Program.cs
+++ C#/pitbull305dale/pitbull305dale/Program.cs
@@ -19,14 +19,24 @@ namespace pitbull305dale
                 }
             WriteLine();
             }
-              ReadKey();
-            for (int i = 0; i < 10; i++)
-            { 
-             for (int j = 0; j < 10; j++)
-            
-            
-            
+             
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Write("*");
+                }
+                WriteLine();
             }
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 1; j == i; j++)
+                {
+                    Write("*");
+                }
+                WriteLine();
+            }
+            ReadKey();
         }
             
     }
